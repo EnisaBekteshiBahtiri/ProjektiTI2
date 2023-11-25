9354999e2869a8f4e43811deb43151872632befa
EnisaBekteshiBahtiri <enisabekteshi97@gmail.com>
2023-11-25 19:07:53 +0100

    Added identity migration and create database in mssql

---
+++ Presentation/Areas/Client/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Presentation.Areas.Client.Models;
+using System.Diagnostics;
+
+namespace Presentation.Areas.Client
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
