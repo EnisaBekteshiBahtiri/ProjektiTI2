90a05f7c0aaf7caf9aeea883c0b6b88118893de9
EnisaBekteshiBahtiri <enisabekteshi97@gmail.com>
2023-11-26 15:26:51 +0100

    login front

--- Presentation/Areas/Client/Controllers/AccountController.cs
+++ Presentation/Areas/Client/Controllers/AccountController.cs
@@ -32,7 +32,19 @@ namespace Presentation.Areas.Client
 
         [HttpGet]
         [AllowAnonymous]
-        public async Task<IActionResult> Login(string returnUrl = null)
+        public async Task<IActionResult> Login(string? returnUrl = null)
+        {
+            // Clear the existing external cookie to ensure a clean login process
+            await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
+
+            ViewData["ReturnUrl"] = returnUrl;
+            return View();
+        }
+
+
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IActionResult> LoginLTE(string? returnUrl = null)
         {
             // Clear the existing external cookie to ensure a clean login process
             await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
