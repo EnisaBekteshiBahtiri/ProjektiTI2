e02dc92c88239e55e3e40d3d984ef34376c148d3
EnisaBekteshiBahtiri <enisabekteshi97@gmail.com>
2023-12-30 18:36:16 +0100

    Users modul implemented

--- Presentation/Areas/Admin/Controllers/UsersController.cs
+++ Presentation/Areas/Admin/Controllers/UsersController.cs
@@ -1,12 +1,45 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ProjektiTI2.App.Interfaces;
 
 namespace Presentation.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class UsersController : Controller
     {
+        private readonly IUserRepository userRepository;
+
+        public UsersController(IUserRepository userRepository)
+        {
+            this.userRepository = userRepository;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult GetUsersJson()
+        {
+            try
+            {
+                var users = userRepository.GetAll();
+
+                var result = users.Select(x => new
+                {
+                    //id = x.Id,
+                    name = x.Name,
+                    surname = x.Surname,
+                    email = x.Email,
+                    emailConfirmed = x.EmailConfirmed,
+                }).ToList();
+
+                return new JsonResult(result);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
     }
 }
